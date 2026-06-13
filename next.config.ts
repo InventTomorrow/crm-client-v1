4fee8f9c3d4de21767add3b6e8593af5538d10a1
abdulwaheed16 <abwaheed.ahmed@gmail.com>
2026-06-13 10:43:24 +0500

    chore (server proxy updated)

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   async rewrites() {
+    const apiOrigin =
+      process.env.NEXT_PUBLIC_API_URL || "http://52.205.151.170";
     return [
       {
-        source: '/api/v1/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://52.205.151.170/api/v1'}/:path*`,
+        source: "/api/v1/:path*",
+        destination: `${apiOrigin}/api/v1/:path*`,
       },
     ];
   },
